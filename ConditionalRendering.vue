1011af796cda036f5584ad94f0324bcb60cf8dc7
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-17 20:21:43 +0530

    Conditoinal Statements

---
+++ ConditionalRendering.vue
@@ -0,0 +1,21 @@
+
+<!-- Conditional Rendering -->
+
+<!-- v-if -->
+
+<h1 v-if="awesome">Hari is awesome!</h1>
+
+<!-- v-else -->
+
+<button @click="awesome = !awesome">Toggle</button>
+
+<h1 v-if="awesome">Hari is awesome!</h1>
+<h1 v-else>Yes Of course he is!</h1>
+
+<!-- v-if on template -->
+
+<template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </template>
