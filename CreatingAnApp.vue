9cdd0b9d9527e402e245f4e7e31b95c0e73ce95a
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-17 19:03:15 +0530

    App Creation and Basic Stuffs

---
+++ CreatingAnApp.vue
@@ -0,0 +1,57 @@
+
+<!-- App Creation -->
+
+import { createApp } from 'vue'
+
+<!-- Root Component -->
+
+import App from './App.vue'
+
+const app = createApp({
+    
+})
+
+<!-- Mounting App -->
+
+<div id="app"></div>
+
+app.mount('#app')
+
+<!-- we can write our root component's 
+template directly inside 
+the mount container -->
+
+<div id="app">
+    <button @click="count++">{{ count }}</button>
+  </div>
+
+  import { createApp } from 'vue'
+  
+  const app = createApp({
+    data() {
+      return {
+        count: 0
+      }
+    }
+  })
+  
+  app.mount('#app')
+
+<!-- Configuration -->
+<!-- For Error Handler -->
+
+app.config.errorHandler = (err) => {
+    /* handle error */
+  }
+
+<!-- There can be Multiple App in one page -->
+
+const app1 = createApp({
+    
+  })
+  app1.mount('#container-1')
+  
+  const app2 = createApp({
+    
+  })
+  app2.mount('#container-2')
