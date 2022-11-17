813566c4e9bfc4cee59fd320ec5ba35dc1256547
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-17 19:43:11 +0530

    Syntax Learning

---
+++ TemplateSyntax.vue
@@ -0,0 +1,42 @@
+
+<!-- The double mustaches -->
+
+<p>Using text interpolation: {{ rawHtml }}</p>
+<p>Using v-html directive: <span v-html="rawHtml"></span></p>
+
+<span style="color: red">This should be red.</span>
+
+<!-- Attribute Bindings -->
+<!-- The v-bind -->
+
+<div v-bind:id="dynamicId"></div>
+
+<!-- Dynamically Binding Multiple Attribute -->
+
+data() {
+  return {
+    objectOfAttrs: {
+      id: 'container',
+      class: 'wrapper'
+    }
+  }
+}
+
+<!-- Directives -->
+
+<p v-if="seen">Now you see me</p>
+
+<a v-bind:href="url"> ... </a>
+
+<a :href="url"> ... </a>
+
+<a v-on:click="doSomething"> ... </a>
+
+<!-- shorthand -->
+<a @click="doSomething"> ... </a>
+
+<a v-on:[eventName]="doSomething"> ... </a>
+
+<a @[eventName]="doSomething">
+
+<form @submit.prevent="onSubmit">...</form>
