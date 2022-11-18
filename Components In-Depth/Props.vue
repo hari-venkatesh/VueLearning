2dac638afff6ebb16cf6b3d86d1d876235d1b20e
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-18 22:48:47 +0530

    Vue Props

---
+++ Components In-Depth/Props.vue
@@ -0,0 +1,49 @@
+
+<!-- Props Declaration -->
+
+<!-- Array Props Declaration -->
+
+export default {
+    props: ['foo'],
+    created() {
+      // props are exposed on `this`
+      console.log(this.foo)
+    }
+  }
+
+  <!-- Object Props Declaration -->
+  
+  export default {
+    props: {
+      title: String,
+      likes: Number
+    }
+  }
+  
+  <!-- Prop Passing Details -->
+
+  export default {
+    props: {
+      greetingMessage: String
+    }
+  }
+  
+  <span>{{ greetingMessage }}</span>
+
+  <MyComponent greeting-message="hello" />
+
+  <!-- Prop Validation -->
+
+  class Person {
+    constructor(firstName, lastName) {
+      this.firstName = firstName
+      this.lastName = lastName
+    }
+  }
+  
+  export default {
+    props: {
+      author: Person
+    }
+  }
+  
