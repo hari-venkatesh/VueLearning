ffca0a0ee68d38de6c322abd7cf6e279d23f301d
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-18 22:27:09 +0530

    Referal's In Vue

---
+++ TemplateRefs.vue
@@ -0,0 +1,77 @@
+
+<!-- Template Refs -->
+
+
+<input ref="input">
+
+<!-- Accessing the Refs -->
+
+<script>
+export default {
+  mounted() {
+    this.$refs.input.focus()
+  }
+}
+</script>
+
+<template>
+  <input ref="input" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        /* ... */
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="items">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  components: {
+    Child
+  },
+  mounted() {
+    // this.$refs.child will hold an instance of <Child />
+  }
+}
+</script>
+
+<template>
+  <Child ref="child" />
+</template>
+
+export default {
+  expose: ['publicData', 'publicMethod'],
+  data() {
+    return {
+      publicData: 'foo',
+      privateData: 'bar'
+    }
+  },
+  methods: {
+    publicMethod() {
+      /* ... */
+    },
+    privateMethod() {
+      /* ... */
+    }
+  }
+}
