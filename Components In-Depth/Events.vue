bb8b38ab3226fd6a48a1d1a2de0fd8da4a6e1122
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-18 22:54:36 +0530

    Emitting and Listening to Events

---
+++ Components In-Depth/Events.vue
@@ -0,0 +1,55 @@
+
+<!-- Emitting and Listening to Events -->
+
+<!-- MyComponent -->
+
+<button @click="$emit('someEvent')">click me</button>
+
+export default {
+    methods: {
+      submit() {
+        this.$emit('someEvent')
+      }
+    }
+  }
+  
+  <!-- Event Arguments -->
+
+  <button @click="$emit('increaseBy', 1)">
+    Increase by 1
+  </button>
+  
+  <MyButton @increase-by="(n) => count += n" />
+
+  <MyButton @increase-by="increaseCount" />
+
+  methods: {
+  increaseCount(n) {
+    this.count += n
+  }
+}
+
+<!-- CustomInput.vue -->
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <input v-model="value" />
+</template>
+
+<MyComponent v-model:title="bookTitle" />
