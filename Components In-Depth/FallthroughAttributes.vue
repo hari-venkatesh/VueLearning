5ccd04b4bcebc4700bd8fb1a79704fc41013aed8
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-18 23:03:40 +0530

    Basic Attributes Of Vue

---
+++ Components In-Depth/FallthroughAttributes.vue
@@ -0,0 +1,39 @@
+
+<!-- Attribute Inheritance -->
+
+<!-- template of <MyButton> -->
+
+    <button>click me</button>
+
+    <MyButton class="large" />
+
+    <!-- Final -->
+
+    <button class="large">click me</button>
+
+<!-- template of <MyButton> -->
+
+    <button class="btn">click me</button>
+
+    <button class="btn large">click me</button>
+
+    <MyButton @click="onClick" />
+
+<!-- template of <MyButton/> that simply renders another component -->
+
+    <BaseButton />
+
+<!-- Disabling Attribute Inheritance -->
+
+<span>Fallthrough attributes: {{ $attrs }}</span>
+
+<div class="btn-wrapper">
+    <button class="btn">click me</button>
+  </div>
+  
+  export default {
+    created() {
+      console.log(this.$attrs)
+    }
+  }
+  
