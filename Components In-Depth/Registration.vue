5c042e0ad0a0f01079f25ebe89b2ba64482ed54b
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-18 22:41:38 +0530

    Component Registration

---
+++ Components In-Depth/Registration.vue
@@ -0,0 +1,56 @@
+
+<!-- Component Registration -->
+
+<!-- Global Registration -->
+
+import { createApp } from 'vue'
+
+const app = createApp({})
+
+app.component(
+  // the registered name
+  'MyComponent',
+  // the implementation
+  {
+    /* ... */
+  }
+)
+
+import MyComponent from './App.vue'
+
+app.component('MyComponent', MyComponent)
+
+app
+  .component('ComponentA', ComponentA)
+  .component('ComponentB', ComponentB)
+  .component('ComponentC', ComponentC)
+
+  <!-- this will work in any component inside the app -->
+
+<ComponentA/>
+<ComponentB/>
+<ComponentC/>
+
+<!-- Local Registration -->
+
+<script>
+import ComponentA from './ComponentA.vue'
+
+export default {
+  components: {
+    ComponentA
+  }
+}
+</script>
+
+<template>
+  <ComponentA />
+</template>
+
+export default {
+    components: {
+      ComponentA: ComponentA
+    }
+    // ...
+  }
+  
