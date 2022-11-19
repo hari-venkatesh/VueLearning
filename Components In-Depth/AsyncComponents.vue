8181b2871f92d42e6dd11ee1750f17bb3952e154
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-19 09:33:26 +0530

    Components State Changing

---
+++ Components In-Depth/AsyncComponents.vue
@@ -0,0 +1,50 @@
+
+<!-- Basci Usage -->
+
+import { defineAsyncComponent } from 'vue'
+
+const AsyncComp = defineAsyncComponent(() => {
+  return new Promise((resolve, reject) => {
+    resolve(/* loaded component */)
+  })
+})
+
+import { defineAsyncComponent } from 'vue'
+
+const AsyncComp = defineAsyncComponent(() =>
+  import('./components/MyComponent.vue')
+)
+
+app.component('MyComponent', defineAsyncComponent(() =>
+  import('./components/MyComponent.vue')
+))
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    AdminPage: defineAsyncComponent(() =>
+      import('./components/AdminPageComponent.vue')
+    )
+  }
+}
+</script>
+
+<template>
+  <AdminPage />
+</template>
+
+<!-- Loading and Error States -->
+
+const AsyncComp = defineAsyncComponent({
+    loader: () => import('./Foo.vue'),
+
+    loadingComponent: LoadingComponent,
+
+    delay: 200,
+
+    errorComponent: ErrorComponent,
+    timeout: 3000
+  })
+  
