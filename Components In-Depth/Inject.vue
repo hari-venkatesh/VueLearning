f7070cbdb9d8217c5e27a997c2498745d8c1c3c6
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-19 09:32:27 +0530

    Prop Injection in Vue

---
+++ Components In-Depth/Inject.vue
@@ -0,0 +1,73 @@
+
+<!-- Prop Drilling -->
+
+export default {
+    provide: {
+      message: 'hello!'
+    }
+  }
+
+  export default {
+    data() {
+      return {
+        message: 'hello!'
+      }
+    },
+    provide() {
+      return {
+        message: this.message
+      }
+    }
+  }
+  
+  <!-- App Level Provide -->
+
+  import { createApp } from 'vue'
+
+const app = createApp({})
+
+app.provide(/* key */ 'message', /* value */ 'hello!')
+
+export default {
+    inject: ['message'],
+    created() {
+      console.log(this.message) // injected value
+    }
+  }
+
+  export default {
+    inject: ['message'],
+    data() {
+      return {
+        // initial data based on injected value
+        fullMessage: this.message
+      }
+    }
+  }
+  
+  <!-- Injection Aliasing -->
+
+  export default {
+    inject: {
+      /* local key */ localMessage: {
+        from: /* injection key */ 'message'
+      }
+    }
+  }
+
+  
+  import { computed } from 'vue'
+
+  export default {
+    data() {
+      return {
+        message: 'hello!'
+      }
+    },
+    provide() {
+      return {
+        message: computed(() => this.message)
+      }
+    }
+  }
+  
